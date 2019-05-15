867cbf0289e7c435cfcabf790e02858d494a9cd6
M Pearson <lissa.pearson@gmail.com>
2019-05-14 22:14:44 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -7,7 +7,7 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Mauna Loa CO2 levels: 1959-present"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar to enter date info
   sidebarLayout(
     sidebarPanel(
       p('Enter a date on or after March 1, 1958 then hit submit to see how average global CO2 has increased since that date.'),
@@ -31,6 +31,6 @@ shinyUI(fluidPage(
   ),
   hr(),
   p("This page utilizes CO2 data provided by NOAA and presented using a Shiny interface for R data. The ui.R and server.R files for
-        the application are available at [github link]"),
+        the application are available at https://github.com/mepearson/MaunaLoa"),
   p("Data from Dr. Pieter Tans, NOAA/ESRL (www.esrl.noaa.gov/gmd/ccgg/trends/) and Dr. Ralph Keeling, Scripps Institution of Oceanography (scrippsco2.ucsd.edu/).")
 ))
